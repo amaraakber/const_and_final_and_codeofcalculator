0b0bd18c137ca0fb6d14bc6b4370dc8d4e3a64b8
Amara Sahar <136242678+amaraakber@users.noreply.github.com>
2023-07-05 12:30:29 +0500

    it is a code of simple calculator
    and the difference between const and final

---
+++ calculator.dart
@@ -0,0 +1,24 @@
+void main() {
+  //it is a code of calculator
+  // var number1 = 34;
+  // var number2 = 23;
+  // print(number1 + number2);
+  // print(number1 - number2);
+  // print(number1 * number2);
+  // print(number1 / number2);
+  // print(number1 ~/ number2);
+  // print(number1 % number2);
+  //const and final
+  // final myAge = 23;
+  // final myname = 'Saniya';
+  //sometime final value is change and the some time final value is not change
+  //myAge = 25;
+  // const myAge = 24;
+  // const myName = "Saniya";
+  // myName = "sajid";
+  //The value of const are not changed
+  //it is a example of difference between final and const
+  // final myDate = DateTime.now();
+  // myDate = DateTime.now();
+  // const date = DateTime.now();
+}
